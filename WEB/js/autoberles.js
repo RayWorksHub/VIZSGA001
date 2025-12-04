a7e5d29a6df4b5f5a2a7c4b9fef9ec49115b2262
RayWorks <csukardi.rajmund@ganziskola.hu>
2025-12-04 20:04:11 +0100

    d

---
+++ WEB/js/autoberles.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    var navCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            navCollapse.classList.remove('show');
+        });
+    });
+});
+
+function closeMenu() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            navCollapse.classList.remove('show');
+        });
+    });
+}
+
